78e8e8d63fff5a0551a2a4c9d4b5de58b6458ea0
houssamelatmani <houssamelatmani01@gmail.com>
2023-10-20 17:33:08 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/top.js
@@ -0,0 +1,44 @@
+import clientPromise from "../lib/mongodb";
+
+export default function Top({movies}){
+    return(
+        <div>
+            <h1>Top 10 Films In 2023</h1>
+            <p>
+                <small>(According to Metacritic)</small>
+            </p>
+            <ul>
+                {
+                    movies.map(movie=>(
+                        <li>
+                            <h2>{movie.title}</h2>
+                            <h3>{movie.metacritic}</h3>
+                            <p>{movie.plot}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+}
+
+
+export async function getStaticProps(){
+    try{
+        const client = await clientPromise;
+        const db = await client.db("sample_mflix")
+
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .sort({metacritic:-1})
+            .limit(1000)
+            .toArray()
+
+            return {
+                props:{movies:JSON.parse(JSON.stringify(movies))},
+            }
+    }catch(err){
+        console.error(err )
+    }
+}
